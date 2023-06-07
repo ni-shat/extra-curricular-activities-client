b58854b69cfa23dc508ccfdb778d6599ba02442d
ni-shat <nishat.tabassum301@gmail.com>
2023-06-07 22:58:24 +0600

    basic layout is made

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,19 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+import Home from "../pages/home/home/Home";
+import Main from "../layout/Main";
+
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        }       
+      ]
+    },
+  ]);
