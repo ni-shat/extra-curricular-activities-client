bb3b503d288b5f4bcd3939c4a19840083129f468
ni-shat <nishat.tabassum301@gmail.com>
2023-06-11 21:28:02 +0600

    .env file, authContext, firebase config are created

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,20 @@ import './index.css'
 import { router } from './routes/Routes.jsx'
 import { RouterProvider } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import AuthProvider from './providers/AuthProvider'
 // import { HelmetProvider } from 'react-helmet-async';
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <HelmetProvider> */}
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-    {/* </HelmetProvider> */}
+    <AuthProvider>
+      {/* <HelmetProvider> */}
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+      {/* </HelmetProvider> */}
+    </AuthProvider>
 
   </React.StrictMode>
 )
--- src/pages/signup/SignUp.jsx
+++ src/pages/signup/SignUp.jsx
@@ -1,58 +1,122 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Navbar from '../shared/navbar/Navbar';
 // import bg from ''
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import ggl from '../../assets/ggl.png'
+import { AuthContext } from '../../providers/AuthProvider';
 
 
 const SignUp = () => {
+
+
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    const { createUser, updateUserProfile } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const onSubmit = data => {
+
+        createUser(data.email, data.password)
+            .then(result => {
+
+                const loggedUser = result.user;
+                console.log(loggedUser);
+
+                updateUserProfile(data.name, data.photoURL)
+                    .then(() => {
+                        const saveUser = { name: data.name, email: data.email }
+                        fetch('http://localhost:5000/users', {
+                            method: 'POST',
+                            headers: {
+                                'content-type': 'application/json'
+                            },
+                            body: JSON.stringify(saveUser)
+                        })
+                            .then(res => res.json())
+                            .then(data => {
+                                if (data.insertedId) {
+                                    reset();
+                                    Swal.fire({
+                                        position: 'top-end',
+                                        icon: 'success',
+                                        title: 'User created successfully.',
+                                        showConfirmButton: false,
+                                        timer: 1500
+                                    });
+                                    navigate('/');
+                                }
+                            })
+
+
+
+                    })
+                    .catch(error => console.log(error))
+            })
+    };
+
+
     return (
         <div className='bg-[../../assets/bg-signup.svg] bg-cover bg-center h-screen'>
-            
-        <h3 className='font-monoton-lg opacity-10 text-gray-500  whitespace-nowrap text-center -rotate-90 absolute -left-52 top-[42%] '>Signup</h3>
-        
-        <Navbar></Navbar>
-        {/* <img src={bg} alt="" /> */}
-        <div className="pt-14  bg-white">
-       
-            <div className="flex w-[80%] mx-auto items-center justify-center ">
-                
-                <div className='w-[65%]'>
-                    <div className="card w-full pb-10  flex-shrink-0 shadow-2xl bg-base-100  ">
-                        <div className="card-body">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
-                            </div>
-                            <div className="form-control mt-6">
-                                <button className="btn btn-primary text-white">Login</button>
-                            </div>
-                            <div className="divider  mt-10">OR</div>
-                            <div className='text-center '>
-                                <Link className='flex text-black  items-center gap-2 btn border-gray-500 bg-white' to='/signup'> <span>Sign in with google</span>  <img className='w-7 h-7' src={ggl} alt="" /></Link>
-                            </div>
-                            <div className='flex mt-10 justify-center'>
-                                <Link to='/signup'>Don't Have an account? <span className='text-red-600 font-bold'>Sign up</span></Link>
-                            </div>
-                            
+
+            <h3 className='font-monoton-lg opacity-10 text-gray-500  whitespace-nowrap text-center -rotate-90 absolute -left-52 top-[42%] '>Signup</h3>
+
+            <Navbar></Navbar>
+            {/* <img src={bg} alt="" /> */}
+            <div className="pt-14  bg-white">
+
+                <div className="flex w-[80%] mx-auto items-center justify-center ">
+
+                    <div className='w-[65%]'>
+                        <div className="card w-full pb-10  flex-shrink-0 shadow-2xl bg-base-100  ">
+                            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Name</span>
+                                    </label>
+                                    <input type="text"  {...register("name", { required: true })} name="name" placeholder="Name" className="input input-bordered" />
+                                    {errors.name && <span className="text-red-600">Name is required</span>}
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Photo URL</span>
+                                    </label>
+                                    <input type="text"  {...register("photoURL", { required: true })} placeholder="Photo URL" className="input input-bordered" />
+                                    {errors.photoURL && <span className="text-red-600">Photo URL is required</span>}
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input type="email"  {...register("email", { required: true })} name="email" placeholder="email" className="input input-bordered" />
+                                    {errors.email && <span className="text-red-600">Email is required</span>}
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Password</span>
+                                    </label>
+                                    <input type="password"  {...register("password", {
+                                        required: true,
+                                        minLength: 6,
+                                        maxLength: 20,
+                                        pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
+                                    })} placeholder="password" className="input input-bordered" />
+                                    {errors.password?.type === 'required' && <p className="text-red-600">Password is required</p>}
+                                    {errors.password?.type === 'minLength' && <p className="text-red-600">Password must be 6 characters</p>}
+                                    {errors.password?.type === 'maxLength' && <p className="text-red-600">Password must be less than 20 characters</p>}
+                                    {errors.password?.type === 'pattern' && <p className="text-red-600">Password must have one Uppercase one lower case, one number and one special character.</p>}
+                                    <label className="label">
+                                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                    </label>
+                                </div>
+                                <div className="form-control mt-6">
+                                    <input className="btn btn-primary" type="submit" value="Sign Up" />
+                                </div>
+                            </form>
                         </div>
                     </div>
-                </div>
 
+                </div>
             </div>
         </div>
-    </div>
     );
 };
 
